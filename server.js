a3ac98121233140be513f3d9c55fddffb3fcb293
ManobhavSachan <manobhavsachan@gmail.com>
2023-12-19 16:45:53 +0530

    CORS

--- server.js
+++ server.js
@@ -1,81 +1,44 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const express = require('express');
+const cors = require('cors'); // Import the cors middleware
 require('dotenv').config();
 
-const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/your_database"; // Default URI if not provided in the environment
-
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/your_database";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Use cors middleware
+app.use(cors());
+
 // Connect to MongoDB and start the server
 async function startServer() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  // ... (unchanged code)
 
-    const db = client.db('SIH');
-    const collection = db.collection('FPO_StateWise');
+  // Define routes
+  app.get('/api/documents', async (req, res) => {
+    const documents = await collection.find().toArray();
+    res.json(documents);
+  });
 
-    // Define routes
+  app.get('/api/distinct-states', async (req, res) => {
+    const distinctStates = await collection.distinct('State Name');
+    res.json(distinctStates);
+  });
 
-    // Route to get all documents in the collection
-    app.get('/api/documents', async (req, res) => {
-      const documents = await collection.find().toArray();
-      res.json(documents);
-    });
+  app.post('/api/distinct-district', express.json(), async (req, res) => {
+    // ... (unchanged code)
+  });
 
-    // Route to get distinct states
-    app.get('/api/distinct-states', async (req, res) => {
-      const distinctStates = await collection.distinct('State Name');
-      res.json(distinctStates);
-    });
+  app.post('/api/fpos', express.json(), async (req, res) => {
+    // ... (unchanged code)
+  });
 
-    app.post('/api/distinct-district', express.json(), async (req, res) => {
-        const state = req.body.state;
-    
-        try {
-          // Find distinct data for the given state
-          const distinctData =  await collection.distinct('District', { "State Name": state });
-    
-          // Send the distinct data as a JSON response
-          res.json(distinctData);
-        } catch (error) {
-          console.error('Error retrieving distinct data:', error);
-          res.status(500).send('Internal Server Error');
-        }
-      });
-
-      app.post('/api/fpos', express.json(), async (req, res) => {
-        const state = req.body.state;
-        const district = req.body.district;
-    
-        try {
-          // Find all data for FPOs in the given state and district
-          const fposData = await collection.find({ "State Name": state, "District": district }).toArray();
-    
-          // Send the FPOs data as a JSON response
-          res.json(fposData);
-        } catch (error) {
-          console.error('Error retrieving FPOs data:', error);
-          res.status(500).send('Internal Server Error');
-        }
-      });
-
-    // Start the server
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-    });
-  } finally {
-    // Close the MongoDB connection when the server stops
-    process.on('SIGINT', async () => {
-      await client.close();
-      console.log('MongoDB connection closed');
-      process.exit();
-    });
-  }
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 }
 
 startServer().catch(console.error);
